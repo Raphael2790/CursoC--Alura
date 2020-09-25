350d24f3f94ea4b0c0dfe047f85a481240a06620
Raphael2790 <rssshev@gmail.com>
2020-09-25 15:11:40 -0300

    Concluido o curso de coleções parte 1

---
+++ ByteBank/TrabalhandoComColeções/Navegador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrabalhandoComColeções
+{
+    internal class Navegador
+    {
+        private string _actual;
+        //Uma stack traduzindo simplesmente é uma pilha, entrando na regras de pilhas como forma de organização
+        //Em uma pilha sempre é removido o elemento do topo LIFO ( Last In First Out)
+        private Stack<string> _historicoAnterior = new Stack<string>();
+        private Stack<string> _historicoPosterior = new Stack<string>();
+        public Navegador()
+        {
+            System.Console.WriteLine("Página atual:" + _actual);
+        }
+
+        //Dada que uma pilha só podemos interagircom o topo, unicos métodos para colocar e remover são
+        //Pop(Remover) e Push(Inserir)
+        internal void navegarPara(string url)
+        {
+            _historicoAnterior.Push(_actual);
+            _actual = url;
+            Console.WriteLine(_actual);
+        }
+
+        internal void Anterior()
+        {
+            if (_historicoAnterior.Any())
+            {
+                _historicoPosterior.Push(_actual);
+                _actual = _historicoAnterior.Pop();
+                Console.WriteLine(_actual);
+            }
+        }
+
+        internal void Proximo()
+        {
+            if (_historicoPosterior.Any())
+            {
+                _historicoAnterior.Push(_actual);
+                _actual = _historicoPosterior.Pop();
+                Console.WriteLine(_actual);
+            }
+        }
+    }
+}
--- ByteBank/TrabalhandoComColeções/Program.cs
+++ ByteBank/TrabalhandoComColeções/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TrabalhandoComColeções
 {
@@ -6,7 +7,51 @@ namespace TrabalhandoComColeções
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Quando usar uma lista ligada? Quando necessário remover mais vezes elementos no meio de um lista
+            //Listas ligadas não possuem indice, buscas, remoções são feitas pelo próprio elemento
+            // Inserções são feitas com base em algum elemento
+            LinkedList<string> daysOfWeek = new LinkedList<string>();
+
+            var d1 = daysOfWeek.AddFirst("domingo");
+            var d7 = daysOfWeek.AddLast("sábado");
+            var d2 = daysOfWeek.AddAfter(d1, "segunda");
+
+            // Não podemos usar for em listas ligadas por falta de um indice
+            foreach(var day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            Navegador navegador = new Navegador();
+            navegador.navegarPara("vazia");
+            navegador.navegarPara("www.google.com");
+            navegador.navegarPara("www.alura.com.br");
+            navegador.navegarPara("www.caelum.com.br");
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Proximo();
+            navegador.Proximo();
+
+            Pedagio pedagio = new Pedagio();
+            pedagio.Enfileirar("van");
+            pedagio.Enfileirar("trator");
+            pedagio.Enfileirar("ônibus viagem");
+            pedagio.Enfileirar("carro familiar");
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
         }
+
+        //Resumindo existem vários tipos de listas em C#, algumas são:
+        //IList - tipo de lista que permite valores duplicados e possue ordem de indexação
+        //ISet - tipo de lista que não permite valores duplicado e não possue ordem
+        //Dictionary - tipo de lista que indexa os elementos por um tipo de chave primária
+        //List - listas comuns
+        //HashSet - Listas com maior indexação, porém mais custosas em memória e buscas com tempo fixo
+        //Queue - Filas seguem a estrutura de algoritmos FIFO 
+        //Stack - Filas que seguem a estrutura de saida e entrada LIFO
+        //Linked List - Listas que são indexadas pelo elementos próximos a ele mesmo
     }
 }
